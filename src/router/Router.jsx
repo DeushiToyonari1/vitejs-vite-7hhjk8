6f226a9f661982a645f784722629793f2e92d9c4
DeushiToyonari1 <zombieship@gmail.com>
2024-04-24 15:42:37 +0900

    Atomic Designをコミット

---
+++ src/router/Router.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Top } from "../components/pages/Top";
+import { Users } from "../components/pages/Users";
+import { Header } from "../components/atoms/layout/Header";
+import { HeaderOnly } from "../components/templates/HeaderOnly";
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Header><Top /></Header>} />
+                <Route path="/users" element={<HeaderOnly><Users /></HeaderOnly>} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
